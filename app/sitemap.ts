26584d22ee727ee2acf7bbd6555ca8bcdbc6d4b8
magzhanserikuly <magzhan.serikuly.kz@gmail.com>
2024-07-27 23:06:58 +0500

    Update sitemap.ts

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,25 +3,25 @@ import { MetadataRoute } from 'next'
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: 'https://adlet-ibraimov.vercel.app',
+      url: 'https://lvniakea.vercel.app/',
       lastModified: new Date(),
       changeFrequency: 'yearly',
       priority: 1,
     },
     {
-      url: 'https://adlet-ibraimov.vercel.app/cv',
+      url: 'https://lvniakea.vercel.app/cv',
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 0.8,
     },
     {
-      url: 'https://adlet-ibraimov.vercel.app/projects',
+      url: 'https://lvniakea.vercel.app/projects',
       lastModified: new Date(),
       changeFrequency: 'weekly',
       priority: 0.7,
     },
     {
-      url: 'https://adlet-ibraimov.vercel.app/contacts',
+      url: 'https://lvniakea.vercel.app/contacts',
       lastModified: new Date(),
       changeFrequency: 'weekly',
       priority: 0.9,

8189cb8a86c1efb5b2335889a947171748003b14
Adlet Ibraimov <ibrvimv@gmail.com>
2025-10-19 14:41:19 +0200

    add vercel analytics

--- app/(site)/layout.tsx
+++ app/(site)/layout.tsx
@@ -4,7 +4,7 @@ import { GoogleAnalytics, GoogleTagManager } from '@next/third-parties/google';
 
 import Header from '../../components/shared/Header';
 import Footer from '../../components/shared/Footer';
-
+import { Analytics } from "@vercel/analytics/next"
 import { IBM_Plex_Mono, Montserrat, Roboto, Inter, Quicksand, Outfit } from 'next/font/google';
 
 import clsx from 'clsx';
@@ -20,7 +20,7 @@ const ibm_plex_mono = Outfit({
 
 export const metadata = {
   title: 'ADLET IBRAIMOV',
-  description: 'Adlet Ibraimov is a Web Developer & Content Creator based in Milan, Italy',
+  description: 'Adlet Ibraimov is a Frontend, Shopify and Web Developer based in Milan, Italy',
 };
 
 export default async function RootLayout({
@@ -42,6 +42,7 @@ export default async function RootLayout({
           <main>{children}</main>
         </div>
         <Footer />
+        <Analytics />
       </body>
       <GoogleAnalytics gaId='G-VG95DB2TKE' />
     </html>
